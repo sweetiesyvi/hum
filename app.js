9386d598b3499ce628c370fdf87e6199e253af0e
sweetiesyvi <sngulungu@una.edu>
2026-01-28 21:25:14 -0700

    port config for render

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.send('Hello World: DEV')
 })
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
