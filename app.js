c6af70c35d885afe3c3801b9ca18dc2b548854d5
sweetie <rineemwky@gmail.com>
2026-02-18 21:44:23 -0700

    Integrate MongoDB and add API endpoints
    Added MongoDB connection and API endpoints for database interaction.

--- app.js
+++ app.js
@@ -2,15 +2,46 @@ import express from 'express'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
-const app = express()
 
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+dotenv.config()
+
+const app = express()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.json())
 
+const uri = process.env.MONGO_URI
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+let db
+
+async function connectDB() {
+  try {
+    await client.connect()
+    await client.db("admin").command({ ping: 1 })
+    console.log("Connected to MongoDB")
+
+    db = client.db("test")
+  } catch (err) {
+    console.error("Mongo error:", err)
+  }
+}
+
+connectDB()
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from API (Checkpoint 2)' })
@@ -22,6 +53,16 @@ app.get('/api/query', (req, res) => {
 
 app.post('/api/body', (req, res) => {
   res.json({ message: `Hello ${req.body.name}` })
+})
+
+
+app.get('/api/db-test', async (req, res) => {
+  try {
+    const data = await db.collection("demo").find().toArray()
+    res.json({ data })
+  } catch (err) {
+    res.status(500).json({ error: "Database error" })
+  }
 })
 
 const PORT = process.env.PORT || 3000
