89cdbcba4465f3aca8e7c586953ea98141a30cd9
sweetie <rineemwky@gmail.com>
2026-03-10 01:36:44 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,68 +15,121 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.json())
 
 const client = new MongoClient(process.env.MONGO_URI)
+
 let db
 
-async function connectDB() {
-  try {
-    await client.connect()
-    db = client.db('school')
-    console.log("MongoDB connected")
-  } catch (err) {
-    console.error("MongoDB connection error:", err)
-  }
+async function connectDB(){
+
+try{
+
+await client.connect()
+
+db = client.db("school")
+
+console.log("MongoDB connected")
+
+}catch(err){
+
+console.error(err)
+
 }
 
+}
+
+connectDB()
+
 /* CREATE */
-app.post('/api/users', async (req, res) => {
-  const { name } = req.body
 
-  await db.collection('users').insertOne({ name })
+app.post('/api/users', async(req,res)=>{
 
-  res.json({ message: "User added" })
+try{
+
+const {name} = req.body
+
+await db.collection("users").insertOne({name})
+
+res.status(201).json({message:"User created"})
+
+}catch(err){
+
+res.status(500).json({error:"database error"})
+
+}
+
 })
 
 /* READ */
-app.get('/api/users', async (req, res) => {
 
-  const users = await db.collection('users')
-    .find()
-    .sort({ _id: -1 })
-    .toArray()
+app.get('/api/users', async(req,res)=>{
 
-  res.json(users)
+try{
+
+const users = await db.collection("users")
+.find()
+.sort({_id:-1})
+.toArray()
+
+res.status(200).json(users)
+
+}catch(err){
+
+res.status(500).json({error:"database error"})
+
+}
+
 })
 
 /* UPDATE */
-app.put('/api/users/:id', async (req, res) => {
 
-  const { id } = req.params
-  const { name } = req.body
+app.put('/api/users/:id', async(req,res)=>{
 
-  await db.collection('users').updateOne(
-    { _id: new ObjectId(id) },
-    { $set: { name } }
-  )
+try{
 
-  res.json({ message: "User updated" })
+const {id} = req.params
+
+const {name} = req.body
+
+await db.collection("users").updateOne(
+{_id:new ObjectId(id)},
+{$set:{name}}
+)
+
+res.status(200).json({message:"User updated"})
+
+}catch(err){
+
+res.status(500).json({error:"update error"})
+
+}
+
 })
 
 /* DELETE */
-app.delete('/api/users/:id', async (req, res) => {
 
-  const { id } = req.params
+app.delete('/api/users/:id', async(req,res)=>{
 
-  await db.collection('users').deleteOne({
-    _id: new ObjectId(id)
-  })
+try{
 
-  res.json({ message: "User deleted" })
+const {id} = req.params
+
+await db.collection("users").deleteOne({
+_id:new ObjectId(id)
+})
+
+res.status(200).json({message:"User deleted"})
+
+}catch(err){
+
+res.status(500).json({error:"delete error"})
+
+}
+
 })
 
 const PORT = process.env.PORT || 3000
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
-  })
+app.listen(PORT,()=>{
+
+console.log("Server running on port",PORT)
+
 })
